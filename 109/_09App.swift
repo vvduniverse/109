4a0554db5bc39dfb472bfa8ec62ecf4769ca0851
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-17 19:47:25 +0200

    Initial Commit

---
+++ 109/_09App.swift
@@ -0,0 +1,17 @@
+//
+//  _09App.swift
+//  109
+//
+//  Created by Vahtee Boo on 17.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct _09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
